2c615d7c31159f3ee8a77d4792c157ee0519804f
Marcos Brandan <marcosbrandan681@gmail.com>
2025-06-12 12:14:22 -0300

    Se hace que el id de la ruta no sea requerido al momento de crear un horario

--- src/models/horario.model.js
+++ src/models/horario.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const horarioSchema = new mongoose.Schema({
-    id_ruta: { type: mongoose.Schema.Types.ObjectId, ref: 'Ruta', required: true },
+    id_ruta: { type: mongoose.Schema.Types.ObjectId, ref: 'Ruta', required: false },
     id_parada: { type: mongoose.Schema.Types.ObjectId, ref: 'Parada', required: true },
     horario: { type: String, required: true } // formato HH:mm
 });
